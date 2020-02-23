f9b78135451edb4fdd9f77049b1050b907eeffa6
Musa406 <bsse0824@iit.du.ac.bd>
2020-02-23 11:32:05 +0600

    modified

--- src/app/services/sections.service.ts
+++ src/app/services/sections.service.ts
@@ -5,13 +5,22 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class SectionsService {
-  strApiParent = 'https://localhost:44337/api/';
 
+  strApiParent = 'https://localhost:44337/api/section/';
   urlApiPath: string;
   constructor(private httpME: HttpClient) { }
 
+  getTechnologyStackData() {
+    return this.httpME.get('../assets/tech.json');
+  }
+  postContactData(contactData: any) {
+    return this.httpME.post(this.strApiParent+'tech', contactData);
+  }
+
+
+
   getSectionContent(apiPath: any) {
-    this.urlApiPath = this.strApiParent + apiPath;
-    return this.httpME.get<any>(this.urlApiPath);
+    // this.urlApiPath = this.strApiParent + apiPath;
+    return this.httpME.get<any>('../assets/items.json');
   }
 }
